3f0352ab480dd43f73f2b3b11ae28653c69fa805
ZoeUttingTecharge <z.utting@ntlworld.com>
2024-11-04 17:12:09 +0000

    Basic conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,18 @@
 function ListGroup() {
     const items = [
-        'New York',
-        'Tokyo',
-        'London',
-        'France',
-        'Ediburgh'
+        // 'New York',
+        // 'Tokyo',
+        // 'London',
+        // 'France',
+        // 'Ediburgh'
 
     ];
 
-    
+
   return (
     <>
         <h1>List</h1>
+        {items.length === 0 && <p>No item found</p>}
         <ul className="list-group">
           {items.map(item => <li key={item} className="list-group-item">{item}</li>)}
         </ul>
