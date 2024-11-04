b2bd680774b18d2bd4fd6a7527337789a29a469f
ZoeUttingTecharge <z.utting@ntlworld.com>
2024-11-04 17:36:44 +0000

    basic event handling

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,20 +1,27 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
     const items = [
-        // 'New York',
-        // 'Tokyo',
-        // 'London',
-        // 'France',
-        // 'Ediburgh'
+        'New York',
+        'Tokyo',
+        'London',
+        'France',
+        'Ediburgh'
 
     ];
 
+    const handleClick = (e: MouseEvent) => console.log(e)
+ 
 
   return (
     <>
         <h1>List</h1>
         {items.length === 0 && <p>No item found</p>}
         <ul className="list-group">
-          {items.map(item => <li key={item} className="list-group-item">{item}</li>)}
+          {items.map((item, index) => <li 
+          key={item}
+          onClick={handleClick}
+          className="list-group-item">{item}</li>)}
         </ul>
     </>
   );
